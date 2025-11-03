5a8f9ea99f96a5a3e380e9018522552f3016fd19
pbbaque <pbbaque@gmail.com>
2025-11-03 20:00:50 +0100

    chore(components): remove unused UI components and update module references
    Deleted obsolete components (breadcrumb, empty-state, loading-spinner, modal, table) and cleaned up their imports from components.module.ts.

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlertComponent } from './alert/alert.component';
-import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { EmptyStateComponent } from './empty-state/empty-state.component';
 import { HeaderComponent } from './header/header.component';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
-import { ModalComponent } from './modal/modal.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { TableComponent } from './table/table.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { ConfirmComponent } from './confirm/confirm.component';
@@ -19,17 +14,12 @@ import { EntityDetailComponent } from './entity-detail/entity-detail.component';
 
 @NgModule({
   declarations: [
-    AlertComponent, 
-    BreadcrumbComponent, 
-    EmptyStateComponent, 
-    HeaderComponent, 
-    LoadingSpinnerComponent, 
-    ModalComponent, 
-    PaginationComponent, 
-    SidebarComponent, 
-    TableComponent, 
-    FooterComponent, 
-    ConfirmComponent, 
+    AlertComponent,
+    HeaderComponent,
+    PaginationComponent,
+    SidebarComponent,
+    FooterComponent,
+    ConfirmComponent,
     EntityDetailComponent
   ],
   imports: [
@@ -39,15 +29,10 @@ import { EntityDetailComponent } from './entity-detail/entity-detail.component';
     ReactiveFormsModule
   ],
   exports: [
-    AlertComponent, 
-    BreadcrumbComponent, 
-    EmptyStateComponent, 
-    HeaderComponent, 
-    LoadingSpinnerComponent, 
-    ModalComponent, 
-    PaginationComponent, 
-    SidebarComponent, 
-    TableComponent,
+    AlertComponent,
+    HeaderComponent,
+    PaginationComponent,
+    SidebarComponent,
     FooterComponent,
     ConfirmComponent,
     FormsModule,
